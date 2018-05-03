7f9de014debea7649cae2124284dfba0b603091e
MurielPinho <muriel.apinho@gmail.com>
2018-05-03 17:28:32 +0100

    certo

--- cwcreator.cpp
+++ cwcreator.cpp
@@ -16,17 +16,8 @@ int  main() {
   beginProgram();
   Dictionary j;
   j.CreateDictionary();
-
-<<<<<<< HEAD
-
   j.showDictionary();
 
-
-
-=======
-  j.showDictionary();
-
->>>>>>> e857b4f619f59faa74bc43c9ebe1532d3c661568
   // while (true)
   // {
   //   switch (options()) {
